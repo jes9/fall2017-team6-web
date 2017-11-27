81f8d8b2b0eaab303c96d5fe59d0ba97a95dc774
Chaldric <aldrich.chris@comcast.net>
2017-11-27 01:03:24 -0500

    Added some example pictures for car select and made adjustments to the diagnostics pages.

--- mirage/config.js
+++ mirage/config.js
@@ -11,7 +11,7 @@ export default function() {
           carmodel: 'X-wing Fighter',
           carmiles: '9994',
           caryear: '2015',
-          carimage: 'https://commons.wikimedia.org/wiki/Main_Page#/media/File:Bee_hummingbird_(Mellisuga_helenae)_immature_male.jpg'
+          carimage: '/assets/images/T70XWing-Fathead.png'
         }
       }, {
         type: 'cars',
@@ -21,7 +21,7 @@ export default function() {
           carmodel: 'TIE Fighter',
           carmiles: '9997',
           caryear: '2077',
-          carimage: 'https://commons.wikimedia.org/wiki/Main_Page#/media/File:Bee_hummingbird_(Mellisuga_helenae)_immature_male.jpg'
+          carimage: '/assets/images/tie-fighter.jpg'
         }
       }, {
         type: 'cars',
@@ -31,7 +31,7 @@ export default function() {
           carmodel: 'Millennium Falcon',
           carmiles: '9999',
           caryear: '1977',
-          carimage: 'https://commons.wikimedia.org/wiki/Main_Page#/media/File:Bee_hummingbird_(Mellisuga_helenae)_immature_male.jpg'
+          carimage: '/assets/images/MillenniumFalcon2-Fathead.png'
         }
       }]
     };
