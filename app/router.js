d0c6d493f084a7c90c0ad0e5290c7dd583ba6436
Christian Aldrich <aldrich.chris@comcast.net>
2017-12-03 14:24:09 -0500

    Was missing });

--- app/router.js
+++ app/router.js
@@ -27,6 +27,7 @@ Router.map(function() {
     this.route('form-review');
     this.route('form-submitted');
     this.route('begin-page');
+  });
   this.route('mechanic');
   this.route('sign-in');
   this.route('sign-up');
